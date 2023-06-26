aa388c9b3276928c8b76be2de656ca421569cfe9
simonl <121858461+simonlesly@users.noreply.github.com>
2023-06-26 17:10:34 +0200

    huitième commit

--- asseca/adherent.php
+++ asseca/adherent.php
@@ -2,14 +2,13 @@
 <html>
 <head>
     <?php 
-        include 'header.php'; 
         require_once('inc/manager-db.php');
         $listeAdherent = getAllAdherent();
     ?>
     <title>Liste Adherents</title>
     <link rel="stylesheet" type="text/css" href="css/adherent.css">
     <li><a href="Cotipaspaye.php">adherent qui on pas payer</a></li>
-    <li><a href="refreshcoti.php">refresh</a></li>
+    <li><a href="index.php">retour à la page précédente</a></li>
 </head>
 
     <h1>Liste des Adhérents</h1>
@@ -18,38 +17,31 @@
           <th>id</th>
           <th>Nom</th>
           <th>Prénom</th>
-          <th>Ville</th>
-          <th>Adresse</th>
           <th>Mail</th>
-          <th>tel</th>
-          <th>login</th>
-          <th>password</th>
-          <th>Fonction</th>
-          <th>Cotisation</th>
-          <th>Cotisation payé ?</th>
-          <th>update</th>
+          <th>Numéro de fixe </th>
+          <th>Numéro de portable</th>
+          <th>Numéro de rue</th>
+          <th>Nom de la rue </th>
+          <th>Code postale</th>
+          <th>Nom de la ville</th>
+          <th>modification</th>
         </tr>
     
 
     <?php foreach ($listeAdherent as $adherent ) :?>
 <tr>
-
     <td><?php echo $adherent->id; ?></td>
     <td><?php echo $adherent->nom; ?></td>
     <td><?php echo $adherent->prenom; ?></td>
-    <td><?php echo $adherent->ville; ?></td>
-    <td><?php echo $adherent->adresse; ?></td>
     <td><?php echo $adherent->mail; ?></td>
-    <td><?php echo $adherent->tel; ?></td>
-    <td><?php echo $adherent->login; ?></td>
-    <td><?php echo $adherent->password; ?></td>
-    <td><?php echo $adherent->fonction; ?></td>
-    <td><?php echo $adherent->cotisation; ?></td>
-    <td><?php echo $adherent->paye; ?>
-    <a href="updateCoti.php?id=<?php echo $adherent->id; ?>" >cliquer ici si la personne a payé</a></td>
-    <td> <a href="updateAdh.php?id=<?php echo $adherent->id; ?>" >update</a></td>
+    <td><?php echo $adherent->NumFixe; ?></td>
+    <td><?php echo $adherent->NumPortable; ?></td>
+    <td><?php echo $adherent->NumeroRue; ?></td>
+    <td><?php echo $adherent->NomRue; ?></td>
+    <td><?php echo $adherent->CodePostale; ?></td>
+    <td><?php echo $adherent->NomVille; ?></td>
+    <td> <a href="formulaire_adh.php?id=<?php echo $adherent->id; ?>" >update</a></td>
 </tr>
 <?php endforeach; ?>
 
 </table>
-<?php
--- asseca/header.php
+++ asseca/header.php
@@ -8,18 +8,23 @@
 <body>
     <div class="header">
         <header>
-            <h1>ASSECA</h1>
+            
             <br>
             <p style="text-align:center"><img src="images/logo.PNG" width="300" height="300"></p>
             </br>
+            <h3>Bienvenue sur le site de l'association ASSECA </h3>
+            <br>
             <nav>
                 <ul>
                     <li><a href="index.php">Accueil</a></li>
-                    <li><a href="adherent.php">Adhérents</a></li>
-                    <li><a href="#">Connexion</a></li>
+                    <li><a href="sport.php">Activités sportives</a></li>
+                    <li><a href="index.php">Activités éducatives</a></li>
+                    <li><a href="index.php">Activités Culturelles</a></li>
+                    <li><a href="adherent.php">liste adhérent</a></li> 
                     <li><a href="inscription.php">Inscription</a></li> 
                 </ul>
             </nav>
+            
         </header>
     </div>
 </body>
